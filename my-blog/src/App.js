08c3b3ce8d792f9d7f20c9a9ae551441e8b4b6c7
DoneGod <cbajpas@gmail.com>
2024-08-06 15:56:43 -0400

    first
    react project nmp

---
+++ my-blog/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Awsome Blog</h1>
+      <div id="page-body">
+          Welcome to my blog!
+      </div>
+    </div>
+  );
+}
+
+export default App;
