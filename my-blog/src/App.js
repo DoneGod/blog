4b8d2b118c4cd128ab028fac10cde70be656d172
DoneGod <cbajpas@gmail.com>
2024-08-06 17:41:20 -0400

    pages
    setting up pages

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlesPages';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awsome Blog</h1>
-      <div id="page-body">
-          Welcome to my blog!
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Awsome Blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+             <Route path="/about" element={<AboutPage />} />
+             <Route path="/articles" element={<ArticlePage />} />
+             <Route path="/articles/:articleId" element={<ArticlesListPage />} />
+          </Routes>
+          
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ my-blog/src/pages/ArticlesListPage.js
@@ -0,0 +1,7 @@
+const ArticlesListPage = () => {
+    return(
+        <h1>This is the Articles list page!</h1>
+    )
+}
+
+export default ArticlesListPage;
