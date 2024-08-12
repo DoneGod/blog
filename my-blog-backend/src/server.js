09a15320486efa4a2c9d3d288ee8865edf98cfa7
DoneGod <cbajpas@gmail.com>
2024-08-11 22:57:07 -0400

    upvote Button
    Add the upvote button, however it is crashing the website

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -29,7 +29,8 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
     const article = await db.collection('articles').findOne({name});
     
     if (article){
-        res.send(`The ${name} article now has ${article.upvotes} upvotes!!`);
+       //res.send(`The ${name} article now has ${article.upvotes} upvotes!!`);
+       res.json(article);
     }else {
         res.send('that article does not exist');
     }
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,18 +1,30 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from 'axios';
 import articles from "./article-content";
+import CommentsList from "../components/CommentsList";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
 
     const [ articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: []});
+    const { articleId } = useParams();
 
     useEffect(() => {
-        setArticleInfo({upvotes: 3, comments: []});
-    });
-
-    const { articleId } = useParams();
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();               
+    }, []);
     const article = articles.find(article => article.name === articleId);
+
+    const addUpvote = async() => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = await response.data;
+        setArticleInfo(updatedArticle);
+    }
 
     if(!article){
         return <NotFoundPage/>
@@ -22,10 +34,14 @@ const ArticlePage = () => {
     return(
         <>
         <h1>{article.title}</h1>
-        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        <div className="upvotes-section">
+            <button onClick={addUpvote}>Upvote</button>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p> 
+        </div> 
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
+        <CommentsList comments={articleInfo.comments}/> 
         </>
     );
 }
